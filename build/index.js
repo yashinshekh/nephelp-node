900f4b6754387339e8eb16e76a1c4c51993b7604
yashinshekh <nyasin585@gmail.com>
2018-10-24 15:21:03 +0545

    #master: cors implemented using es6

--- build/index.js
+++ build/index.js
@@ -1,18 +1,22 @@
-"use strict";
+'use strict';
 
-var _express = require("express");
+var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
-var _routes = require("./routes");
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
+var _routes = require('./routes');
 
 var _routes2 = _interopRequireDefault(_routes);
 
-var _mongoose = require("./config/mongoose");
+var _mongoose = require('./config/mongoose');
 
 var _mongoose2 = _interopRequireDefault(_mongoose);
 
-var _bodyParser = require("body-parser");
+var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
@@ -22,20 +26,15 @@ var app = (0, _express2.default)();
 
 (0, _mongoose2.default)();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 app.use(_bodyParser2.default.json());
+app.use(_cors2.default);
 
 (0, _routes2.default)(app);
 
 var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
-    return console.log("Server running on port " + port);
+  return console.log('Server running on port ' + port);
 });
 //# sourceMappingURL=index.js.map
