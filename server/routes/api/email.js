9072a21ec4d9f359991d9d66d6d2d3f1fdae1731
yashinshekh <nyasin585@gmail.com>
2018-10-25 11:52:55 +0545

    #master: email sending part completed

---
+++ server/routes/api/email.js
@@ -0,0 +1,39 @@
+import {Router} from 'express';
+import nodemailer from 'nodemailer';
+
+const router = Router();
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'nephelp9@gmail.com',
+        pass: 'yasin123456'
+    }
+});
+
+router.post('/',(req,res) => {
+    const errors = {};
+    let mailOptions = {
+        from: 'nephelp9@gmail.com',
+        to: 'nyasin585@gmail.com',
+        subject: req.body.subject,
+        // text: req.body.description,
+        html: `<p>Hello,</p>
+               <p>
+                ${req.body.description}
+               </p>
+               <p>Regards,<br>NepHelp (nephelp9@gmail.com)</p>
+            `
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            error.message = 'There was error while sending email.';
+            return res.status(404).json(error);
+        }
+
+        info(res.status(200).json({emailmessage:"Message successfully sent."}));
+    });
+});
+
+export default router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import user from './api/user';
 import profile from './api/profile';
+import email from './api/email';
 
 const routeSchema = (app) => {
     app.use('/api/user',user);
-    app.use('/api/profile',profile)
+    app.use('/api/profile',profile);
+    app.use('/api/sendemail',email);
 };
 
 export default routeSchema;
